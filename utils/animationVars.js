16e517f5be12b191068afbb01aea5ed609ef3892
RezaVAULT3D <100597768+RezaVAULT3D@users.noreply.github.com>
2022-04-24 15:37:35 +0900

    animationVars

---
+++ utils/animationVars.js
@@ -0,0 +1,55 @@
+export const varFadeInUp = {
+  hidden: {
+    y: -30,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4
+    },
+  }
+};
+
+export const varFadeInDown = {
+  hidden: {
+    y: 30,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4
+    },
+  }
+};
+
+export const varFadeInLeft = {
+  hidden: {
+    x: -30,
+    opacity: 0
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4
+    },
+  }
+};
+
+export const varFadeInRight = {
+  hidden: {
+    x: 30,
+    opacity: 0
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4
+    },
+  }
+};
